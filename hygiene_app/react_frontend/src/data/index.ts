83bb07be1f53d1ba9ef4ff18f736eadf5937dc27
Shohei Kanno <foolhouse0513@gmail.com>
2025-08-05 09:25:56 +0900

    フォルダ名変更

---
+++ hygiene_app/react_frontend/src/data/index.ts
@@ -0,0 +1,12 @@
+// データ
+export * from "./mockEmployees";
+export * from "./mockRecords";
+export * from "./mockRecordItems";
+export * from "./hygieneCategories";
+export * from "./mockBranches";
+
+// 型
+export type { Employee } from "./mockEmployees";
+export type { Record } from "./mockRecords";
+export type { RecordItem } from "./mockRecordItems";
+export type { CategoryDefinition } from "./hygieneCategories";
