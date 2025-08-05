35c908f7050a70887560bd999d5feaf0d0401873
Shohei Kanno <foolhouse0513@gmail.com>
2025-08-05 10:23:12 +0900

    営業所追加

--- hygiene_app/react_frontend/src/data/index.ts
+++ hygiene_app/react_frontend/src/data/index.ts
@@ -10,3 +10,4 @@ export type { Employee } from "./mockEmployees";
 export type { Record } from "./mockRecords";
 export type { RecordItem } from "./mockRecordItems";
 export type { CategoryDefinition } from "./hygieneCategories";
+export type { Branch } from "./mockBranches";
